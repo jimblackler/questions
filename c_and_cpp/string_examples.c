b45345d39c9789b3bef387c67475b9e1f28a79fa
Jim Blackler <jimblackler@google.com>
2014-05-03 11:16:51 +0100

    C version

---
+++ c_and_cpp/string_examples.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, const char * argv[]) {
+
+  // Make a string
+  char *s = "Hello";
+
+  // Find length of a string
+  printf("%zu\n", strlen(s));
+
+  // Compare two strings
+  char *s2 = "Hello";
+  printf("%d\n", strcmp(s, s2));
+
+  // Search for a substring
+  s2 = "el";
+  printf("%zu\n", strstr(s, s2) - s);
+
+  return 0;
+}
